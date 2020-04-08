dda986b22edf7fe89acf8e3c25c62f4d9110df30
anwachuku15 <anwachuku15@gmail.com>
2020-04-08 16:38:47 -0400

    set up reducers and store

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// REDUX
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </Provider>
   );
 }
 
