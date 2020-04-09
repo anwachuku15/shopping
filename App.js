964bb35be89c7df4c3bbc0de7206bd991bf6d4a1
anwachuku15 <anwachuku15@gmail.com>
2020-04-08 22:16:20 -0400

    format StatusBar & ProductDetailScreen

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native'
+import Colors from './constants/Colors'
 // REDUX
 import { Provider } from 'react-redux'
 import store from './redux/store';
 
 // NAVIGATION
 import ShopNavigator from './navigation/ShopNavigator'
+
+
+StatusBar.setBarStyle('dark-content')
 
 export default function App() {
   return (
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,14 +9,22 @@ import {
 } from 'react-native'
 // REDUX
 import { useSelector } from 'react-redux'
+import Colors from '../../constants/Colors'
 
 const ProductDetailScreen = props => {
     const productId = props.navigation.getParam('productId')
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{uri: selectedProduct.imageUrl}}/>
+
+            <View style={styles.buttons}>
+                <Button title='Add to Cart' color={Colors.primary} onPress={() => {}} />
+            </View>
+
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 }
 
@@ -27,7 +35,26 @@ ProductDetailScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: '100%',
+        height: 300
+    },
+    buttons: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        marginVertical: 20,
+        marginHorizontal: 20,
+        textAlign: 'center',
+    },
 })
 
 export default ProductDetailScreen
