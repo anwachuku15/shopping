977dc3916427a9b9ac3c84b0f783d5c539d00537
anwachuku15 <anwachuku15@gmail.com>
2020-04-10 20:06:17 -0400

    email/password signup auth

---
+++ redux/actions/authActions.js
@@ -0,0 +1,31 @@
+
+export const SIGNUP = 'SIGNUP'
+
+
+export const signup = (email, password) => {
+    return async dispatch => {
+        const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjFDet9PN8mZjani67TVYKumPfqouGQyE',
+        {
+            method: 'POST',
+            headesr: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+                returnSecureToken: true
+            })
+        })
+
+        if (!res.ok) {
+            throw new Error('Something went wrong')
+        }
+        
+        const resData = await res.json()
+        console.log(resData)
+        
+        dispatch({
+            type: SIGNUP,
+        })
+    }
+}
--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,13 +1,78 @@
-import React from 'react'
+import React, { useReducer, useCallback } from 'react'
 import { ScrollView, View, KeyboardAvoidingView, StyleSheet, Button } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
+
+import { useDispatch } from 'react-redux'
+import { signup } from '../../redux/actions/authActions'
 
 import Input from '../../components/UI/Input'
 import Card from '../../components/UI/Card'
 import Colors from '../../constants/Colors'
 
 
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE'
+// FORM VALIDATION REDUCER
+const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+
+        const updatedValues = {
+            ...state.inputValues,
+            [action.input]: action.value
+        }
+        const updatedValidities = {
+            ...state.inputValidities,
+            [action.input]: action.isValid
+        }
+        let updatedFormIsValid = true
+        for (const key in updatedValidities) {
+            updatedFormIsValid = updatedFormIsValid && updatedValidities[key]
+        }
+        
+        let newValues = {}
+        for (const key in updatedValues) {
+            newValues[key] = updatedValues[key].trimStart()
+        }
+
+        return {
+            ...state,
+            formIsValid: updatedFormIsValid,
+            inputValues: newValues,
+            inputValidities: updatedValidities,
+        }
+    }
+    return state
+}
+
 const AuthScreen = props => {
+    const dispatch = useDispatch()
+
+     // FORM VALIDATION - INITIAL STATE
+     const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+            email: '',
+            password: ''
+        }, 
+        inputValidities: {
+            email: false,
+            password: false
+        }, 
+        formIsValid: false,
+    })
+
+    const signupHandler = () => {
+        dispatch(signup(formState.inputValues.email, formState.inputValues.password))
+    }   
+
+    const inputChangeHandler = useCallback(
+        (inputType, inputValue, inputValidity) => {
+            dispatchFormState({
+                type: FORM_INPUT_UPDATE,
+                value: inputValue,
+                input: inputType,
+                isValid: inputValidity,
+            })
+    }, [dispatchFormState])
+
     return (
         <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={50} style={styles.screen}>
             <LinearGradient colors={[Colors.primary, Colors.blacksmoke]} style={styles.gradient}>
@@ -17,11 +82,11 @@ const AuthScreen = props => {
                             id='email' 
                             label='E-mail' 
                             keyboardType='email-address'
-                            required 
                             email 
+                            required 
                             autoCapitalize='none' 
-                            errorMessage='Please enter a valid email address' 
-                            onInputChange={() => {}}
+                            errorText='Please enter a valid email address' 
+                            onInputChange={inputChangeHandler}
                             initialValue=''
                         />
                         <Input 
@@ -32,15 +97,15 @@ const AuthScreen = props => {
                             required 
                             minLength={8} 
                             autoCapitalize='none' 
-                            errorMessage='Please enter a valid password' 
-                            onInputChange={() => {}}
+                            errorText='Please enter a valid password, at least 8 characters' 
+                            onInputChange={inputChangeHandler}
                             initialValue=''
                         />
                         <View style={styles.buttonContainer}>
                             <Button 
                                 title='Login' 
                                 color={Colors.primary}
-                                onPress={() => {}}
+                                onPress={signupHandler}
                             />
                         </View>
                         <View style={styles.buttonContainer}>
